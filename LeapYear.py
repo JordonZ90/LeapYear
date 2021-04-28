8cfc6b75e0ab46cadf41dd2b004c98412ed74751
Vik <jzenterprize90@gmail.com>
2021-04-27 21:54:06 -0400

    Basic leap year function

---
+++ LeapYear.py
@@ -0,0 +1,8 @@
+def leap_year():
+    year = int(input("Please enter the year "))
+    if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
+        print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
+
+leap_year()
